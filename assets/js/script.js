bb73452f0a44962541dcb91d3f87b6e186edd79b
Abi Baydarus <x@bydrz.com>
2025-02-24 18:20:14 +0700

    add loading screen

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(() => {
+    document.getElementById("loading-screen").classList.add("hidden");
+  }, 2000); // Loading akan hilang setelah 2 detik
+});
