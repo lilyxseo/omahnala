c8c5c904400a95517c9f4d6d5f66895bfe64070d
Abi Baydarus <x@bydrz.com>
2025-02-25 01:02:49 +0700

    add section gallery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
   setTimeout(() => {
     document.getElementById("loading-screen").classList.add("hidden");
-  }, 2000); // Loading akan hilang setelah 2 detik
+  }, 1000); // Loading akan hilang setelah 2 detik
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var galleryItems = document.querySelectorAll(".gallery-item");
+    var lightboxImage = document.getElementById("lightboxImage");
+
+    galleryItems.forEach(function (item) {
+        item.addEventListener("click", function () {
+            var imgSrc = item.getAttribute("data-img");
+            lightboxImage.setAttribute("src", imgSrc);
+        });
+    });
 });
